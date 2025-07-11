fe9936579c0f3e024ad3dbb9cfa379a1bb7213b5
WyvernLang <98944595+WyvernCoder@users.noreply.github.com>
2025-07-11 14:14:18 +0800

    Update thumbnail.py
     - Added processing percentage. 
     - Added support for .heic .mp4 and .dng format.

--- thumbnail.py
+++ thumbnail.py
@@ -2,8 +2,27 @@
 此脚本用于将旧照片生成pho读取的缩略图
 """
 import os
+import pathlib
+
+import rawpy
 from PIL import Image, ImageOps
+from pillow_heif import register_heif_opener
 from datetime import datetime
+from moviepy import VideoFileClip
+
+
+# 获取目录下的总文件数量
+def count_files_by_types(root_dir):
+    count = 0
+    for root, _, files in os.walk(root_dir):
+        if ".thumbnail" in root:
+            continue
+
+        for file in files:
+            if file.lower().endswith(tuple(ext.lower() for ext in supported_extensions)):
+                count += 1
+
+    return count
 
 
 # 获取照片的拍摄日期
@@ -26,30 +45,47 @@ def get_img_datatime(file_path):
 
 
 def generate_thumbnail(photo_folder, thumbnail_size=(200, 200)):
+    # 为 Pillow 启动 heic 图片格式支持
+    register_heif_opener()
+
+    # 计数支持
+    total_files = count_files_by_types(photo_folder)
+    file_count = 0
+
     # 遍历文件夹中的每个日期文件夹
     for root, dirs, files in os.walk(photo_folder):
         # 过滤缩略图文件夹不处理
         if ".thumbnail" in root:
             continue
+
         for file in files:
+            # 增加计数器
+            if pathlib.Path(os.path.join(root, file)).suffix in supported_extensions:
+                file_count += 1
+
             # 获取图片文件路径
             file_path = os.path.join(root, file)
 
-            # 仅处理图片文件
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                # 获取照片的拍摄日期
-                date_time = get_img_datatime(file_path)
-                # 生成缩略图的文件夹路径
-                thumbnail_folder = os.path.join(photo_folder, ".thumbnail", str(date_time.year),
-                                                str(date_time.month).zfill(2), str(date_time.day).zfill(2))
-                if not os.path.exists(thumbnail_folder):
-                    os.makedirs(thumbnail_folder)
-                # 缩略图照片的路径
-                thumbnail_path = os.path.join(thumbnail_folder, file)
-                # 缩略图存在则跳过不处理
-                if os.path.exists(thumbnail_path):
-                    continue
+            # 获取照片的拍摄日期
+            date_time = get_img_datatime(file_path)
 
+            # 生成缩略图的文件夹路径
+            thumbnail_folder = os.path.join(photo_folder, ".thumbnail", str(date_time.year),
+                                            str(date_time.month).zfill(2), str(date_time.day).zfill(2))
+            if not os.path.exists(thumbnail_folder):
+                os.makedirs(thumbnail_folder)
+            # 缩略图照片的路径
+            thumbnail_path = os.path.join(thumbnail_folder, file)
+            # 将缩略图路径的后缀设置为jpg
+            path = pathlib.Path(thumbnail_path)
+            thumbnail_path = path.with_suffix(".jpg")
+            # 缩略图存在则跳过不处理
+            if os.path.exists(thumbnail_path):
+                print('[%s%%] Exists %s' % (int(file_count / total_files * 100), thumbnail_path))
+                continue
+
+            # 处理常见图片文件
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
                 # 生成缩略图并保存
                 try:
                     with Image.open(file_path) as img:
@@ -57,10 +93,54 @@ def generate_thumbnail(photo_folder, thumbnail_size=(200, 200)):
                         img.thumbnail(thumbnail_size)
                         img = img.convert("RGB")
                         img.save(thumbnail_path)
-                        print(thumbnail_path)
+                        print('[%s%%] Created %s' % (int(file_count / total_files * 100), thumbnail_path))
                 except OSError as e:
                     print(f"Error processing {file}: {e}")
 
+            # 处理 heic 图片文件
+            if file.lower().endswith('.heic'):
+                # 生成缩略图并保存
+                try:
+                    with Image.open(file_path) as img:
+                        img = ImageOps.exif_transpose(img)
+                        img.thumbnail(thumbnail_size)
+                        img = img.convert("RGB")
+                        img.save(thumbnail_path)
+                        print('[%s%%] Created %s' % (int(file_count / total_files * 100), thumbnail_path))
+                except OSError as e:
+                    print(f"Error processing {file}: {e}")
+
+            # 处理视频文件
+            if file.lower().endswith('.mp4'):
+                # 生成缩略图并保存
+                try:
+                    with VideoFileClip(file_path) as clip:
+                        frame = clip.get_frame(clip.duration / 2)
+                        img = Image.fromarray(frame)
+                        img.thumbnail(thumbnail_size)
+                        img = img.convert("RGB")
+                        img.save(thumbnail_path)
+                        print('[%s%%] Created %s' % (int(file_count / total_files * 100), thumbnail_path))
+                except OSError as e:
+                    print(f"Error processing {file}: {e}")
+
+            # 处理常见图片文件
+            if file.lower().endswith('dng'):
+                # 生成缩略图并保存
+                try:
+                    with rawpy.imread(file_path) as raw:
+                        standard_rgb = raw.postprocess(use_camera_wb=True)
+                        img = Image.fromarray(standard_rgb)
+                        img.thumbnail(thumbnail_size)
+                        img = img.convert("RGB")
+                        img.save(thumbnail_path)
+                        print('[%s%%] Created %s' % (int(file_count / total_files * 100), thumbnail_path))
+                except OSError as e:
+                    print(f"Error processing {file}: {e}")
+
+
+# 定义支持的文件
+supported_extensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.heic', '.mp4', '.dng']
 
 # 定义照片文件夹和缩略图尺寸
 photo_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "Completed")
